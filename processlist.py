e0a5536c026d64b0dce9406e213a4b9f2f83de1f
Tianw22 <33138468+Tianw22@users.noreply.github.com>
2018-11-29 00:23:59 -0500

    Update processlist.py

--- processlist.py
+++ processlist.py
@@ -8,11 +8,33 @@
 import pandas as pd
 import numpy as np
 
-file_name =  "newprevue.xlsx" #newprevue.xlsx就是用excel处理后的表格。
-newsheet = pd.read_excel(io=file_name2)
-newsheet = newsheet.dropna() #drop all the na lines. 去掉所有带na值的行。
+file_name2 =  "prealllinks.xlsx"#prealllinks.xlsx就是用excel处理后的表格。
+newsheet = pd.ExcelFile(io=file_name2)
+newsheet = newsheet.parse('Sheet4')
+#newsheet = newsheet[['count','title']]#only select the columns needed. 选择列。
+#newsheet
 
-writer = pd.ExcelWriter('prevuensize.xlsx')
-newsheet.to_excel(writer,'Sheet1')
+newsheet = newsheet.dropna()#drop all the na lines. 去掉所有带na值的行。
+#newsheet.index
+newsheet.reset_index(drop=True)
+newsheet.set_index('title', inplace=True)#reset index from 0. 从0重新定义index。
+#newsheet
+
+file_name3 =  "film17.xlsx"
+newsheet2 = pd.ExcelFile(io=file_name3)
+newsheet2 = newsheet2.parse('Sheet1')
+newsheet2.columns = ['link','detail','grade','title']
+
+newsheet2.reset_index()
+newsheet2.set_index('title', inplace=True)
+#newsheet2
+
+#用title列进行合并。combine the two dataframe by colomn"title".
+result = pd.concat([newsheet, newsheet2],axis=1, join_axes=[newsheet.index])
+#result
+
+writer = pd.ExcelWriter('filmalllink.xlsx')
+result.to_excel(writer,'Sheet1')
 writer.save()
-#save into a new '.xlsx' for further uses.
+#save into a new sheet of 'filmalllinks.xlsx' for further uses.
+#this dataset has film's page link, its grade and the count of its prevues.
