cdf4459ca0a9b1401d7574f70c68cea17b3e717c
Tianw22 <33138468+Tianw22@users.noreply.github.com>
2018-12-04 01:59:31 -0500

    Create resize.py

---
+++ resize.py
@@ -0,0 +1,11 @@
+#resize all the merged pics into 600*400
+#把文件夹中所有图片缩放到统一尺寸。
+
+from PIL import Image 
+import os
+
+for i in range(0,n):#n is the number of pics.
+    im = Image.open('./merge/%i.png'%i) #So my dir is ./merge
+    out = im.resize((600,400))
+    out.save(os.path.join('./merge',os.path.basename('./merge/%i.png')%i)) #overwrite the original pics.
+    print('%i is ready'%i)
